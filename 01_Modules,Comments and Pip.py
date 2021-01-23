8024b48154605be768cea66323c4981d7f6e55e1
Revanth <revanthprogrammer@gmail.com>
2021-01-23 16:21:11 +0530

    Initial commit

---
+++ 01_Modules,Comments and Pip.py
@@ -0,0 +1,44 @@
+# print() function is used to print anything
+print("This is an example of print function")
+
+# Modules are the code written by somebody else that we can import in our project and use the functions in it
+import os #imports a module called os. Os is a built in module because you don't have to install it through pip
+# Here is a mind mapping of types of modules
+'''
+                                |Modules|
+                                    |
+------------------------------------ -----------------------------------------
+        |                                                           |
+    |Built-in|                                                  |External|
+'''
+
+#Comments are the lines of code that are ignored by the interpreter - This is a single line comment
+'''
+This is a
+multi-line
+comment
+'''
+#Here is a mind mapping of types of comments
+'''
+                                |Comments|
+                                    |
+------------------------------------ -----------------------------------------
+        |                                                           |
+    |Single Line|                                              |Multi-line|
+'''
+
+# Pip is the package manager of Python that helps in installing modules
+
+# Here is a mind mapping of the Chapter
+
+'''
+                                |Python|
+                                    |
+------------------------------------ -----------------------------------------
+        |                                                           |
+    |Modules|                                                  |Comments|
+        |                                                           |
+    --------------------------------                  -----------------------------------------
+    |                              |                  |                                       |
+|Built-in|                      |External|       |Single line|                          |Multi-line|
+'''
